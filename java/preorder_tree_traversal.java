2ffb490bfd84a31e63f410e647d8a949a544ec69
Ishita Chourasia <ishitach@users.noreply.github.com>
2019-04-11 23:13:33 +0530

    Create preorder_tree_traversal.java

---
+++ java/preorder_tree_traversal.java
@@ -0,0 +1,46 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node{
+	int data;
+	Node left, right;
+	
+	public Node(int key){
+		data = key;
+		left=null;
+		right=null;
+	}
+}
+
+class BT{
+	Node root;
+	BT(int key){
+		root = new Node(key);
+	}
+	
+	BT(){
+		root = null;
+	}
+	
+	public static void main(String[] args){
+		BT bt1=new BT(5);
+		bt1.root.left = new Node(4);
+		bt1.root.right = new Node(5);
+		bt1.root.right.left = new Node(6);
+		preorder(bt1.root);
+	}
+	
+	public static void preorder(Node node)  
+    { 
+        if(node == null){
+        	return;
+        }
+        System.out.println(node.data);
+        preorder(node.left);
+        preorder(node.right);
+        
+    } 
+	
+}
